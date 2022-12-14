7a30179a3ea2712b78592fafbdacb2f4d1fef221
Sumit <115337317+skmishraRoot@users.noreply.github.com>
2022-12-14 18:59:22 +0530

    Adding link

--- LanguageDetector.py
+++ LanguageDetector.py
@@ -1,3 +1,4 @@
+# Need to setup detect before using this for help link:https://pypi.org/project/detect/
 # Importing Language detector form detect module
 from langdetect import detect
 # Taking user input
